129e56ce7a451da4b1c174f6f7d9b02b80cf380d
LuizKramer <luizeduardockramer@gmail.com>
2021-10-03 11:32:25 -0300

    Adiciona leitor de Matriz

--- Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
+++ Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int l, c, i, j;
+    scanf("%d %d", &l, &c);
+    int M[l][c];
+    for(i=0;i<l;i++){
+        for(j=0;j<c;j++){
+            scanf("%d", &M[i][j]);
+        }
+    }
+     for(i=0;i<l;i++){
+        for(j=0;j<c;j++){
+            printf("%d ", M[i][j]);
+        }
+        printf("\n");
+    }
 }
