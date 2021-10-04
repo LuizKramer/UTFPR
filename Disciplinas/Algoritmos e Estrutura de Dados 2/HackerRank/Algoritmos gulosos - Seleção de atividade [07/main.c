718dc9f8c7652d19b74fdd74072cb30aa01ccf40
LuizKramer <luizeduardockramer@gmail.com>
2021-10-04 10:27:13 -0300

    Reimplementa bubbleSort

--- Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - Seleção de atividade [07/main.c
+++ Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - Seleção de atividade [07/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+void bubblesort(int *horaInicio, int *horaFim, int n);
+void metodoGuloso(int *horaInicio, int *horaFim, int n);
+
+void bubblesort(int *horaInicio, int *horaFim, int n){
+    int aux,i,j;
+    for(i=0; i<n; i++){
+        for(j=0; j<n-1-i; j++){
+            if(horaFim[j]>horaFim[j+1]){
+                    aux = horaFim[j];
+                    horaFim[j] = horaFim[j+1];
+                    horaFim[j+1] = aux;
+                    aux = horaInicio[j];
+                    horaInicio[j] = horaInicio[j+1];
+                    horaInicio[j+1] = aux;
+            }
+        }
+    }
+}
 
 void metodoGuloso(int *horaInicio, int *horaFim, int n){
     int aulasAlocadas[n], i,a,b;
@@ -16,8 +34,8 @@ void metodoGuloso(int *horaInicio, int *horaFim, int n){
     printf("Aulas alocadas: ");
     for(i=0; i<a; i++)
         printf("%d ", aulasAlocadas[i]);
-
 }
+
 int main()
 {
    int n, i;
@@ -26,5 +44,6 @@ int main()
    for(i=0; i<n; i++){
         scanf("%d %d", &horaInicio[i], &horaFim[i]);
    }
+   bubblesort(horaInicio,horaFim,n);
    metodoGuloso(horaInicio,horaFim,n);
 }
