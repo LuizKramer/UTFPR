f45e0818bee93f2fe02b11cdd8e937d8a4943fa7
LuizKramer <luizeduardockramer@gmail.com>
2021-10-04 16:50:32 -0300

    Adiciona mapeamento e posição futura

--- Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
+++ Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int metodoGuloso(int l, int c, int M[l][c]){
-    int i,j;
-    int pos = M[0][0];
-    int v[4] = {0,0,0,0};
+
+int metodoGuloso(int l, int c, int M[l][c])
+{
+    int i,j, a = 0;
+    int aux=100;
+    int iProx;
+    int jProx;
+    i=0;
+    j=0;
+
+
+    int posPrev[2]= {l,c};
+    while(i!= l && j !=c){
+        posPrev[0] = iProx;
+        posPrev[1] = jProx;
+        for(a=0; a<4; a++){
+            //mapeamento da posição
+            if(a == 0){i--;}
+            if(a == 1){i++;j--;}
+            if(a == 2){j+=2;}
+            if(a == 3){i++;j--;}
+
+            if(i>=0 && i<=5 && j>=0 && j<=5){
+                printf("%d%d: [%d]\n",i, j, M[i][j]);
+                if(i != posPrev[0] && j!= posPrev[2]){
+                    if(M[i][j] <= aux){
+                        aux = M[i][j];
+                        iProx = i;
+                        jProx = j;
+                    }
+                }
+            }
+        }
+
+        i = iProx;
+        j = jProx;
+        printf("\nPROX %d%d\n", iProx, jProx);
+    }
+    return aux;
 }
 
 int main()
@@ -12,10 +47,10 @@ int main()
     int l, c, i, j;
     scanf("%d %d", &l, &c);
     int M[l][c];
-    for(i=0;i<l;i++){
-        for(j=0;j<c;j++){
+    for(i=0; i<l; i++){
+        for(j=0; j<c; j++){
             scanf("%d", &M[i][j]);
         }
     }
-    metodoGuloso(l,c,M);
+    printf("\n%d", metodoGuloso(l,c,M));
 }
