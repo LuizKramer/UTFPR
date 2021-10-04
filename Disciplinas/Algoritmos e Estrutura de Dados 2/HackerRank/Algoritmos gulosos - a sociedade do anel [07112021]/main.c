bb2f2eb30ca8ce40e33a48ff38ce92d92cef535d
LuizKramer <luizeduardockramer@gmail.com>
2021-10-04 17:32:51 -0300

    Problema em achar pos Prévia

--- Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
+++ Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
@@ -4,28 +4,32 @@
 
 int metodoGuloso(int l, int c, int M[l][c])
 {
-    int i,j, a = 0;
+    int i =0;
+    int j =0;
+    int a = 0;
     int aux=100;
-    int iProx;
-    int jProx;
+    int iProx =0;
+    int jProx =0;
+    int iPrev =0;
+    int jPrev =0;
     i=0;
     j=0;
+    int    b = 0;
+    while(b <10){
 
-
-    int posPrev[2]= {l,c};
-    while(i!= l && j !=c){
-        posPrev[0] = iProx;
-        posPrev[1] = jProx;
-        for(a=0; a<4; a++){
+        for(a=0; a<5; a++){
             //mapeamento da posição
-            if(a == 0){i--;}
-            if(a == 1){i++;j--;}
-            if(a == 2){j+=2;}
-            if(a == 3){i++;j--;}
+
+
+            if(a == 1){i--;}
+            if(a == 2){i++;j--;}
+            if(a == 3){j+=2;}
+            if(a == 4){i++;j--;}
 
             if(i>=0 && i<=5 && j>=0 && j<=5){
-                printf("%d%d: [%d]\n",i, j, M[i][j]);
-                if(i != posPrev[0] && j!= posPrev[2]){
+                printf("%d = %d\n",(i+1)*(j+2),(iPrev+1)*(jPrev+2));
+                if((i+1)*(j+2) != (iPrev+1)*(jPrev+2)){
+                    printf("%d%d: [%d]\n",i, j, M[i][j]);
                     if(M[i][j] <= aux){
                         aux = M[i][j];
                         iProx = i;
@@ -33,11 +37,16 @@ int metodoGuloso(int l, int c, int M[l][c])
                     }
                 }
             }
+            if(a == 0){
+                iPrev = i;
+                jPrev = j;
+                printf("\nPREV %d%d\n", i, j);
+            }
         }
-
         i = iProx;
         j = jProx;
         printf("\nPROX %d%d\n", iProx, jProx);
+        b++;
     }
     return aux;
 }
