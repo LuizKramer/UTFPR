2e134cf2949c69b05f56c7528cef8e3a3e36a9d3
LuizKramer <luizeduardockramer@gmail.com>
2021-10-05 21:55:44 -0300

    Cansei codigo não funciona

--- Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
+++ Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int verificaPrev(int *iPrev, int *jPrev, int i, int j, int d, int e){
+    int a,b;
+    for(a=0; a<=d ;a++){
+        for(b=0; b<=e; b++){
+            printf("\niPrev: %d jPrev: %d \ni: %d j: %d \neq1:%d == eq2:%d\nd: %d e: %d\n",iPrev[a], jPrev[b],(iPrev[a]+1)*(jPrev[b]+2),(i+1)*(j+2), i,j, d,e);
+            if((iPrev[a]+1)*(jPrev[b]+2) == (i+1)*(j+2)){
+                printf("Return 0\n");
+                return 0;
+            }
+            else{
+                printf("Return 1\n");
+            }
+        }
+    }
+}
 
 int metodoGuloso(int l, int c, int M[l][c])
 {
-    int i =0;
-    int j =0;
-    int a = 0;
-    int aux=100;
-    int iProx =0;
-    int jProx =0;
-    int iPrev =0;
-    int jPrev =0;
-    i=0;
-    j=0;
-    int    b = 0;
-    while(b <10){
+    int i,j, a, aux, iProx, jProx, d, e, b;
+    j=0 , a, aux, iProx =0;
+    i = 0;
+    jProx, d=0;
+    e=0, b = 0;
+    aux=100;
+    int iPrev[l], jPrev[c];
+    iPrev[0]= 0;
+    jPrev[0]= 0;
+    printf("\n %d %d \n", d, e);
+    while(b <6){
         aux = 100;
-
         for(a=0; a<5; a++){
             //mapeamento da posição
             if(a == 0){i--;}
@@ -26,13 +39,12 @@ int metodoGuloso(int l, int c, int M[l][c])
             if(a == 3){i++;j--;}
             if(a == 4){
                 i--;
-                iPrev = i;
-                jPrev = j;
-                printf("\niPrev: %d  jPrev:%d\n", iPrev, jPrev);
+                iPrev[d] = i;
+                printf("\nIprev mG:%d\n", iPrev[d]);
+                jPrev[e] = j;
             };
-
             if(i>=0 && i<=5 && j>=0 && j<=5 && a!=4){
-                if(!(i == iPrev && j==jPrev)){
+                if(verificaPrev(&iPrev[0], &jPrev[0], i, j, d, e)==1){
                     printf("%d%d: [%d] <= %d\n",i, j, M[i][j], aux);
                     if(M[i][j] <= aux){
                         aux = M[i][j];
@@ -42,6 +54,9 @@ int metodoGuloso(int l, int c, int M[l][c])
                 }
             }
         }
+
+        d++;
+        e++;
         i = iProx;
         j = jProx;
         printf("\nPROX %d%d\n", iProx, jProx);
