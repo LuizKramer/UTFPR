64ca45239f89d621e7677873c342ed420022c97b
LuizKramer <luizeduardockramer@gmail.com>
2021-10-08 11:10:38 -0300

    Volta ao codigo do mapeamento

--- Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
+++ Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
@@ -1,68 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int verificaPrev(int *iPrev, int *jPrev, int i, int j, int d, int e){
-    int a,b;
-    for(a=0; a<=d ;a++){
-        for(b=0; b<=e; b++){
-            printf("\niPrev: %d jPrev: %d \ni: %d j: %d \neq1:%d == eq2:%d\nd: %d e: %d\n",iPrev[a], jPrev[b],(iPrev[a]+1)*(jPrev[b]+2),(i+1)*(j+2), i,j, d,e);
-            if((iPrev[a]+1)*(jPrev[b]+2) == (i+1)*(j+2)){
-                printf("Return 0\n");
-                return 0;
-            }
-            else{
-                printf("Return 1\n");
-            }
-        }
-    }
-}
 
 int metodoGuloso(int l, int c, int M[l][c])
 {
-    int i,j, a, aux, iProx, jProx, d, e, b;
-    j=0 , a, aux, iProx =0;
-    i = 0;
-    jProx, d=0;
-    e=0, b = 0;
-    aux=100;
-    int iPrev[l], jPrev[c];
-    iPrev[0]= 0;
-    jPrev[0]= 0;
-    printf("\n %d %d \n", d, e);
-    while(b <6){
-        aux = 100;
-        for(a=0; a<5; a++){
+    int i,j, a = 0;
+    int aux=100;
+    i=4;
+    j=2;
+
+
+    int posPrev[2]= {l,c};
+        for(a=0; a<4; a++){
             //mapeamento da posição
             if(a == 0){i--;}
             if(a == 1){i++;j--;}
             if(a == 2){j+=2;}
             if(a == 3){i++;j--;}
-            if(a == 4){
-                i--;
-                iPrev[d] = i;
-                printf("\nIprev mG:%d\n", iPrev[d]);
-                jPrev[e] = j;
-            };
-            if(i>=0 && i<=5 && j>=0 && j<=5 && a!=4){
-                if(verificaPrev(&iPrev[0], &jPrev[0], i, j, d, e)==1){
-                    printf("%d%d: [%d] <= %d\n",i, j, M[i][j], aux);
-                    if(M[i][j] <= aux){
-                        aux = M[i][j];
-                        iProx = i;
-                        jProx = j;
-                    }
-                }
+
+            if(i>=0 && i<=5 && j>=0 && j<=5){
+                printf("%d%d: [%d]\n",i, j, M[i][j]);
             }
         }
-
-        d++;
-        e++;
-        i = iProx;
-        j = jProx;
-        printf("\nPROX %d%d\n", iProx, jProx);
-
-        b++;
-    }
     return aux;
 }
 
