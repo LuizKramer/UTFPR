43e6f9bd2c78a386ab80859d16414d0f6554ee94
LuizKramer <luizeduardockramer@gmail.com>
2021-10-08 16:14:51 -0300

    Adiciona lista resolvida e altera algoritmo

--- Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
+++ Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Algoritmos gulosos - a sociedade do anel [07112021]/main.c
@@ -5,11 +5,8 @@
 int metodoGuloso(int l, int c, int M[l][c])
 {
     int i,j, a = 0;
-    int aux=100;
     i=4;
     j=2;
-
-
     int posPrev[2]= {l,c};
         for(a=0; a<4; a++){
             //mapeamento da posição
@@ -22,7 +19,7 @@ int metodoGuloso(int l, int c, int M[l][c])
                 printf("%d%d: [%d]\n",i, j, M[i][j]);
             }
         }
-    return aux;
+    return 0;
 }
 
 int main()
