8db3057dd3d853ebae02f714485a36abe3e5b400
LuizKramer <luizeduardockramer@gmail.com>
2021-10-27 13:45:05 -0300

    Finaliza algoritmo

--- Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Menor valor/main.c
+++ Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Menor valor/main.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-#define max(a, b) a < b ? a : b
+#define min(a, b) a < b ? a : b
 
 int maxDQ(int v[], int ini, int fim){
     int m, a, b;
-    if ((fim - ini) <= 1)
-        return max(v[ini], v[fim]);
-    else{
-    m = (ini + fim) / 2;
-    a = maxDQ(v, ini, m);
-    b = maxDQ(v, m + 1, fim);
+    if ((fim - ini) <= 1){
+        return min(v[ini], v[fim]);
     }
-    return max(a, b);
+    else{
+        m = (ini + fim) / 2;
+        a = maxDQ(v, ini, m);
+        b = maxDQ(v, m + 1, fim);
+    }
+    return min(a,b);
 }
 
 int main(){
-    int v[] = {10, -1 , 2, 55, 1, -2, -3, 4, 6, -9,0};
-    printf("%d\n", maxDQ(v, 0, 10));
-    return 0;
+    int n;
+    scanf("%d", &n);
+    int v[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &v[i]);
+    }
+    for(int i=0; i<n; i++){
+        if(maxDQ(v, 0, n-1) == v[i]){
+            printf("%d", i);
+            i = n;
+        }
+    }
 }
