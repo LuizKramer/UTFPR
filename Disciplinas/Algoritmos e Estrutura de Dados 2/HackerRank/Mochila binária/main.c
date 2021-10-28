7abd44b7ab5c57c76ea0c8513a869a92d5b52f30
LuizKramer <luizeduardockramer@gmail.com>
2021-10-28 20:29:58 -0300

    Finaliza

--- Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Mochila binária/main.c
+++ Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Mochila binária/main.c
@@ -2,30 +2,38 @@
 #include <stdlib.h>
 # define max(a,b) a > b ? a : b
 
-int progD(int qtd, int *v, int *p, int cap){
-    int usa, naoUsa;
-    if(m[qtd][cap]==-1){
-        if(p[qtd]>cap){
-            m[qtd][cap] = progD(qtd-1, v, p, cap ,m);
-        }
-        else{
-            usa = v[qtd] + progD(qtd-1, v, p, cap-p[qtd] ,m);
-            naoUsa = progD(qtd-1, v, p,cap, m);
-            m[qtd][cap] = max(usa, naoUsa);
-        }
+int progD(int qtd, int *v, int *p, int cap)
+{
+    int i, j, a, b;
 
+    int m[qtd + 1][cap + 1];
+
+    for (i = 0; i <= qtd; i++)
+    {
+        for (j = 0; j <= cap; j++)
+        {
+            if (i == 0 || j == 0)
+                m[i][j] = 0;
+            else if (p[i - 1] <= j){
+                a = v[i - 1] + m[i - 1][j - p[i - 1]];
+                b = m[i - 1][j];
+                m[i][j] = max(a, b);
+            }
+            else
+                m[i][j] = m[i - 1][j];
+        }
     }
+
     return m[qtd][cap];
-
-
 }
-
 
 int main()
 {
     int capacidade, qtdObj, i, j;
     scanf("%d %d", &capacidade, &qtdObj);
-    int peso[qtdObj], valor[qtdObj], m[qtdObj][capacidade];
+
+    int peso[qtdObj], valor[qtdObj];
+
     for(i = 0; i<qtdObj; i++){
         scanf("%d %d", &peso[i], &valor[i]);
     }
