df5a367c11c3aa142590d9e8a44711c08c29327f
LuizKramer <luizeduardockramer@gmail.com>
2022-08-19 11:30:33 -0300

    Começa Algoritmo

---
+++ Disciplinas/Algoritmos e Estrutura de Dados 2/HackerRank/Agoritmos gulosos - sequenciamento de tarefas em uma máquina/greedy.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+typedef struct{
+    int id; // identificador da tarefa, que será a i-ésima tarefa lida
+    int e; // tempo de execução
+    int d; // deadline
+}tarefas;
+
+
+void greedy(tarefas * a, int N){
+    int v[N];
+    for(int i=0; i<N; i++){
+        v[i] = a[i].d - a[i].e;
+    }
+
+    for(int i=0; i<N; i++){
+        printf("%d ", v[i]);
+    }
+      
+}
+
+
+int main(){
+    int N, i;
+    scanf("%d", &N);
+    tarefas tarefa[N];
+    for(i=0; i < N; i++){
+        tarefa[i].id = i;
+        scanf("%d", &tarefa[i].e);
+        scanf("%d", &tarefa[i].d);
+    }
+
+    greedy(tarefa, N);    
+}
